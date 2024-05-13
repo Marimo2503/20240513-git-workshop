edfb666c395f9556591d8e8c9f0dc9ab6b95b494
yuto <145116214+Marimo2503@users.noreply.github.com>
2024-05-13 15:56:59 +0900

    ブロックの色の変更
    blueをskyblueに変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'coral', 'teal', 'purple'
+    'cyan', 'orange', 'skyblue', 'yellow', 'coral', 'teal', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
